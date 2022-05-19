e3f9b8dc2faa6a2386e74ce1e0baac38b0f94eab
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-19 11:47:18 -0300

    Reading PDF file cover and generating image with PDF.js

--- src/controller/DocumentPreviwController.js
+++ src/controller/DocumentPreviwController.js
@@ -1,3 +1,12 @@
+import { U } from 'pdfjs-dist';
+
+// fazendo querimento da lib pdf instalada 
+const pdfjsLib = require('pdfjs-dist')
+// biblioteca para percore diretorio
+const path = require('path')
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = path.resolve(__dirname, '../../dist/pdf.worker.bundle.js')
+
 export class DocumentPreviwController {
 
 
@@ -12,14 +21,15 @@ export class DocumentPreviwController {
 
         return new Promise((s, f) => {
 
-            switch (this._file.type) {
+            let reader = new FileReader();
 
+            switch (this._file.type) {
                 // caso para verifcica qual image e e executar ação
                 case 'image/png':
                 case 'image/jpg':
                 case 'image/jpeg':
                 case 'image/gif':
-                    let reader = new FileReader();
+
                     reader.onload = e => {
 
                         // promise sucesses
@@ -39,6 +49,60 @@ export class DocumentPreviwController {
 
                 // caso para pdf
                 case 'application/pdf':
+                    // fazendo leitura pdf
+                    reader.onload = e => {
+
+                        // convertendo o arquvio em 8 bits
+                        pdfjsLib.getDocument(new Uint8Array(reader.result)).then(pdf => {
+
+                            // para pega a pagina pdf e o q a promesa retorno
+                            pdf.getPage(1).then(page => {
+
+                                // pegando o view port do pdf
+                                let viewport = page.getViewport(1);
+
+                                // setando canvas
+                                let canvas = document.createElement('canvas')
+                                // definido o contedxto do canvas
+                                let canvasContext = canvas.getContext('2d')
+                                // toda vez que trabalha com canva tem que definir o container 
+                                canvas.width = viewport.width;
+                                canvas.height = viewport.height;
+
+                                page.render({
+                                    canvasContext,
+                                    viewport,
+                                }).then(() => {
+
+                                    // verificando se ttem mas paginas e adicionado o S
+                                    let _s = (pdf.numPages > 1) ? 's' : ''
+
+                                    // mostrando na tela img pdf
+                                    s({
+                                        src: canvas.toDataURL('image/png'),
+                                        info: `${pdf.numPages} pagina${_s}`
+                                    })
+
+                                }).catch(err => {
+
+                                    f(err);
+                                });
+
+                            }).catch(err => {
+
+                                f(err)
+                            });
+
+
+                        }).catch(err => {
+
+                            f(err)
+
+                        });
+
+                    };
+                    reader.readAsArrayBuffer(this._file);
+
                     console.log('pdf foi')
                     break;
 
